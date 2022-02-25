475f240b783fa6b059c90c909c3a2b802e23b912
davidphoon <david.phoon@gmail.com>
2022-02-25 22:28:46 +1100

    First Commit

--- matplot/matplot/urls.py
+++ matplot/matplot/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include('matplot_app.urls'))
+	path('admin/', admin.site.urls),
+	path("", include('matplot_app.urls')),
+	path('aboutme/', include('matplot_app.urls'))
 ]
--- matplot/matplot_app/urls.py
+++ matplot/matplot_app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name ="home")
+    path('', views.homes, name ="homes"),
+	path('aboutme', views.aboutme, name ="aboutme")
 
 
 ]
